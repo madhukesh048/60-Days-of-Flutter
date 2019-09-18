9211c01115d5b2fb62bdffe2a29718de697ce4b2
Madhukesh <madhukesh04@gmail.com>
2019-09-18 12:51:09 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/sixty_days_of_flutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sixty_days_of_flutter
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
